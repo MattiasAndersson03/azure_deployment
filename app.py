1610da62218b6e80bb4dbceab9f1f99fa65de78b
Mattias Andersson <mattias0306@outlook.com>
2025-09-07 22:36:28 +0200

    Remove Azure deployment workflows and update Flask app welcome message

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def index():
-    return "Hello from Azure via Flask! 🚀"
+    return "<center><h1>Welcome to the Flask App! Azure</h1></center>"
+
+if __name__ == "__main__":
+    app.run()
 
